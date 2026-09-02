0c0230ca8f39c139e5ca295d364685a76e4c8afe
Erlend Bergeng <Erlend@lab.it.kvn.no>
2026-09-02 10:29:05 +0200

    start på gjettespill

--- intro.py
+++ intro.py
@@ -1,7 +1,17 @@
-navn = input("hva er ditt navn? ")
-alder = int(input("hvor gammel er du? ")) 
+import random
+# generer ett random tall mellom 0 – 1000
+rettTall = random.randint(0, 1000)
 
-if alder >= 18:
-    print("yey du kan kjøpe øl")
-else:
-    print("du er ikke gammel nok til å kjøpe øl")
+
+# 10 forsøk
+for x in range(1,11):
+    gjett = int(input("gjett ett tall mellom 0 og 1000: "))
+    if gjett == rettTall:
+        print("Hurra! du har gjettet rett tall")
+    else:
+        print("du har gjettet feil tall, prøv på nytt")
+        print("du har", 10 - x, "forsøk igjenn")
+    #print som sier hvor mange forsøk som er igjenn  
+    # hvis rett tall, print gratulerer du har vunnet
+
+# print du har tapt hvis alle forsøk er brukt opp
